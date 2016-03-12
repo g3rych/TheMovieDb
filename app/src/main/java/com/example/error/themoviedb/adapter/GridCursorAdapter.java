58cef3f535ca44e863003b7016f102594919c888
Error <you@localhost.com>
2016-03-12 21:20:56 +0900

    migrate to glide

--- app/src/main/java/com/example/error/themoviedb/adapter/GridCursorAdapter.java
+++ app/src/main/java/com/example/error/themoviedb/adapter/GridCursorAdapter.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 import com.example.error.themoviedb.R;
-import com.squareup.picasso.Picasso;
+
 
 public class GridCursorAdapter extends CursorAdapter {
+    public static int _id = 0;
+    public static int movie_id = 1;
+    public static int title = 2;
+    public static int plot = 3;
+    public static int poster = 4;
+    public static int rating = 5;
+    public static int release_date = 6;
 
     public GridCursorAdapter(Context context,Cursor cursor,int flags) {
         super(context,cursor,flags);
@@ -21,7 +30,9 @@ public class GridCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         ImageView img = (ImageView) view.findViewById(R.id.poster_image_view);
-        Picasso.with(context).load(cursor.getString(4)).into(img);
+
+        Glide.with(context).load(cursor.getString(4)).crossFade().into(img);
+
 
     }
 
